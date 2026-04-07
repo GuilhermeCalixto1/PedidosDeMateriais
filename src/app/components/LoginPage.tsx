97b773ff573af71f8421bb817c55056183a70b15
Guilherme dos Santos Calixto <guilhermegbm.gc@gmail.com>
2026-04-07 19:43:17 +0000

    feat: update placeholder text for matrícula input field

--- src/app/components/LoginPage.tsx
+++ src/app/components/LoginPage.tsx
@@ -59,7 +59,7 @@ export function LoginPage() {
                 <Label>Matrícula</Label>
                 <Input
                   type="text"
-                  placeholder="Ex: 12345"
+                  placeholder="Ex: CSO123 | abc123"
                   value={matricula}
                   onChange={(e) => setMatricula(e.target.value)}
                   disabled={carregando}
